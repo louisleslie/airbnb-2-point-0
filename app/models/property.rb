1b9552528c6083ab25cc9083bc94bb1e697eace9
jake howlett <jake.howlett93@gmail.com>
2021-02-23 12:45:51 +0000

    add name validation to property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,6 @@
 class Property < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
   validates :property_type, presence: true
   validates :total_occupancy, presence: true, numericality: { only_integer: true }
   validates :summary, presence: true
