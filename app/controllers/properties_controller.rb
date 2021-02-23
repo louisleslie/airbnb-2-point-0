2f252207e4d932d65e93f2e11151a7f970155bb3
yunusfirat <yunusfirat44@gmail.com>
2021-02-23 12:14:24 +0000

    Some method has been added

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,10 +1,25 @@
 class PropertiesController < ApplicationController
-  def new
-  end
-
-  def show
-  end
+  before_action :set_property
 
   def index
+    @properties = Property.all
+  end
+
+  def edit
+  end
+
+  def update
+    @property.update(property_params)
+    redirect_to property_path(@property)
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:longitude, :address, :latitude, :has_internet, :has_heating, :has_aircon, :has_kitchen, :has_tv, :price_per_night, :summary, :total_bathrooms, :total_bedrooms, :total_occupancy, :property_type)
   end
 end
