2b47d8410d998f5f25fb47749c0853e7772e411b
jake howlett <jake.howlett93@gmail.com>
2021-02-23 11:50:51 +0000

    add property model validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,8 @@
 class Property < ApplicationRecord
   belongs_to :user
+  validates :property_type, presence: true
+  validates :total_occupancy, presence: true, numericality: { only_integer: true }
+  validates :summary, presence: true
+  validates :price_per_night, presence: true, numericality: { only_integer: true }
+  validates :address, presence: true
 end
