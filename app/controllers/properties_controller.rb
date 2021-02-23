7315dfd883114f49c4a258527c00b26d0f97e7d6
jake howlett <jake.howlett93@gmail.com>
2021-02-23 13:58:51 +0000

    add create method

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,9 +3,28 @@ class PropertiesController < ApplicationController
     @property = Property.new
   end
 
+  def create
+    @property = Property.new(property_params)
+    @property.name = @property.name.capitalize
+    @property.save # TODO swap for if statment when show page is made
+    # if @property.save
+    #   redirect_to property_path(@property)
+    # else
+    #   render :new
+    # end
+  end
+
   def show
   end
 
   def index
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:property_type, :total_occupancy, :total_bedrooms, :total_bathrooms,
+                                     :summary, :price_per_night, :has_tv, :has_kitchen, :has_aircon,
+                                     :has_heating, :has_internet, :address)
   end
 end
