f5f9fbbbc46989d6d99a77c11898b44fe8afbd5b
Alex Terenda <alexterenda@gmail.com>
2021-02-24 08:33:02 +0000

    Added controller methods, view and form partial changes for edit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,15 @@
 class BookingsController < ApplicationController
-  
+  before_action :set_booking, only: [:show, :edit, :destroy]
+
   def new # Louis
+    @booking = Booking.new
   end
 
   def create # Louis
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.property_id = params[:property_id]
+    @booking.save
   end
 
   def show # Yunus
@@ -13,11 +19,40 @@ class BookingsController < ApplicationController
   end
 
   def update # Alex
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   def index # Jake
   end
 
   def destroy # Yunus
-  end 
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_cost, :price_per_night, :number_of_guests,
+                                    :status)
+  end
 end
+
+
+# def new # Louis
+#     @booking = Booking.new
+#   end
+
+#   def create # Louis
+#     @booking = Booking.new(booking_params)
+#     @booking.user = current_user
+#     @booking.property_id = params[:property_id]
+#     if @booking.save
+#       redirect_to booking_path(@booking)
+#     else
+#       render properties_path(params[:property_id])
+#     end
+#   end
