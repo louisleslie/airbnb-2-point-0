4f375863c558e491cd1e7b1fed0be8ba30b36bf3
Louis Leslie <louis.leslie@gmail.com>
2021-02-24 10:53:26 +0000

    Changed property and user models to destroy dependents when they are removed. Added additional data to show page.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,13 +1,13 @@
 class Property < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, :dependent :destroy
   has_many_attached :photos
+  geocoded_by :address
   validates :name, presence: true
   validates :property_type, presence: true
   validates :total_occupancy, presence: true, numericality: { only_integer: true }
   validates :summary, presence: true
   validates :price_per_night, presence: true, numericality: { only_float: true }
   validates :address, presence: true
-  geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
