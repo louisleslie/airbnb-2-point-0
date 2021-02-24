0ad2466553d0986dfdf3ab58cf19359f8d5be7d2
jake howlett <jake.howlett93@gmail.com>
2021-02-24 11:10:13 +0000

    removed raise left over

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -9,7 +9,6 @@ class PropertiesController < ApplicationController
     @property = Property.new(property_params)
     @property.name = @property.name.capitalize
     @property.user = current_user
-    raise
     @property.save # TODO swap for if statment when show page is made
     # if @property.save
     #   redirect_to property_path(@property)
