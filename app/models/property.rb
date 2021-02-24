8970462eed0607e68b312a61757bfbf10e491816
Louis Leslie <louis.leslie@gmail.com>
2021-02-24 11:04:44 +0000

    Corrected error in destroy bit of models. continued html edits to show page

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
-  has_many :bookings, :dependent :destroy
-  has_many_attached :photos
+  has_many :bookings, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
   geocoded_by :address
   validates :name, presence: true
   validates :property_type, presence: true
