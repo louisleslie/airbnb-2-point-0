9c1254709243ae440dcf1dce78579122200c55af
Louis Leslie <louis.leslie@gmail.com>
2021-02-24 07:24:44 +0000

    Added Validations to Bookings that now work.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :property
-  validates :start_date, presence: true, numericality: { greater_than_or_equal_to: Date.today.to_f }
-  validates :end_date, presence: true, numericality: { greater_than: :start_date.to_f }
-  validates :number_of_guests, presence: true, numericality: { less_than_or_equal_to: :property.total_occupancy }
+  validates :start_date, presence: true, numericality: { greater_than_or_equal_to: Date.today.to_time.to_f }
+  validates :end_date, presence: true, numericality: { greater_than: :start_date }
+  validates :number_of_guests, presence: true # , numericality: { less_than_or_equal_to: :property.total_occupancy }
   validates :status, inclusion: { in: %w(Pending Approved Declined), message: "%{value} is not a valid value" }, allow_nil: true
 end
