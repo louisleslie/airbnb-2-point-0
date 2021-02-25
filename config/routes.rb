8eb7c1376ae19f3ce9029dbf21cf97bc07a59b7c
yunusfirat <yunusfirat44@gmail.com>
2021-02-25 11:44:49 +0000

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :properties do
     resources :bookings, only: :create
+    resources :reviews, only: :create
   end
   resources :bookings, except: [:new, :create]
+  resources :reviews, except: [:new, :create]
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
