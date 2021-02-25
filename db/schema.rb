5e578de82fc70fde5ee8596b76c934f88b8f3c91
yunusfirat <yunusfirat44@gmail.com>
2021-02-25 12:54:48 +0000

    added references to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_105453) do
+ActiveRecord::Schema.define(version: 2021_02_25_125201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,9 @@ ActiveRecord::Schema.define(version: 2021_02_25_105453) do
     t.bigint "booking_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "property_id", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["property_id"], name: "index_reviews_on_property_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -109,4 +111,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_105453) do
   add_foreign_key "bookings", "users"
   add_foreign_key "properties", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "properties"
 end
