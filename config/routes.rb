adae2333b7afadb41a0f4242d1e3347916fc0250
yunusfirat <yunusfirat44@gmail.com>
2021-02-25 12:30:41 +0000

    updated review routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   resources :properties do
     resources :bookings, only: :create
   end
-  resources :bookings, only: :create do 
-  resources :reviews, only: :create 
+  resources :bookings, except: [:new, :create] do 
+     resources :reviews, only: :create 
   end
-  resources :bookings, except: [:new, :create]
+ 
   resources :reviews, except: [:new, :create]
   devise_for :users
-  root to: "pages#home"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "users/:user_id/bookings", to: "bookings#users_index", as: :user_bookings
   get "users/:user_id/properties", to: "properties#users_index", as: :user_properties
