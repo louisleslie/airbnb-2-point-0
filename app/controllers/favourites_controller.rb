08e0e09a9b2e731231372dc8e51f62cfd656349e
Louis <louis.leslie@gmail.com>
2021-02-26 11:23:58 +0000

    Revert "Js favourites toggle"

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,16 +1,10 @@
 class FavouritesController < ApplicationController
   def index
-    @favourites = Favourite.select("property_id").where(user_id: current_user.id)
+    @favourites = Favourite.where(user_id: current_user.id)
     
-    @properties = Property.where(id: @favourites)
-    
-    @markers = @properties.geocoded.map do |property|
-      {
-        lat: property.latitude,
-        lng: property.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { property: property }),
-        image_url: helpers.asset_path("map-pin.svg")
-      }
+    @properties = []
+    @favourites.each do |fav|
+      @properties << fav.property
     end
     render 'properties/index'
   end
